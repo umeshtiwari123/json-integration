1d78875843114505fd121c7e3b821c0c1fcbd30d
umeshtiwarijavadeveloper <umeshtiwarijavadeveloper@gmail.com>
2024-02-23 08:41:13 +0530

    json-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'json-read-example';
+export class AppComponent implements OnInit{
+  public userInfo : any;
+  public constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    const url: string = '/assets/data.json';
+    this.http.get(url).subscribe((response) =>{
+      this.userInfo = response;
+    });
+  }
+  // title = 'json-read-example';
 }
